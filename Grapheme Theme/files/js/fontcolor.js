4318cf4d054e52d82df22ba085cf0b4d0dbd10a5
Carlos Vega <dnaatwork@gmail.com>
2014-11-24 08:34:48 -0700

    Beta 1 -
    First upload.

---
+++ Grapheme Theme/files/js/fontcolor.js
@@ -0,0 +1,151 @@
+/*! jQuery Highlighter JS selected*/
+$(document).ready(function () {
+    //characters: 'abcdefhijklmnopqrstuvwzyz'
+    $("body c").highlight("a", {
+        wordsOnly: false,
+        className: 'a'
+    });
+    $("body c").highlight("b", {
+        wordsOnly: false,
+        className: 'b'
+    });
+    $("body c").highlight("c", {
+        wordsOnly: false,
+        className: 'c'
+    });
+    $("body c").highlight("d", {
+        wordsOnly: false,
+        className: 'd'
+    });
+    $("body c").highlight("e", {
+        wordsOnly: false,
+        className: 'e'
+    });
+    $("body c").highlight("f", {
+        wordsOnly: false,
+        className: 'f'
+    });
+    $("body c").highlight("g", {
+        wordsOnly: false,
+        className: 'g'
+    });
+    $("body c").highlight("h", {
+        wordsOnly: false,
+        className: 'h'
+    });
+    $("body c").highlight("i", {
+        wordsOnly: false,
+        className: 'i'
+    });
+    $("body c").highlight("j", {
+        wordsOnly: false,
+        className: 'j'
+    });
+    $("body c").highlight("k", {
+        wordsOnly: false,
+        className: 'k'
+    });
+    $("body c").highlight("l", {
+        wordsOnly: false,
+        className: 'l'
+    });
+    $("body c").highlight("m", {
+        wordsOnly: false,
+        className: 'm'
+    });
+    $("body c").highlight("n", {
+        wordsOnly: false,
+        className: 'n'
+    });
+    $("body c").highlight("o", {
+        wordsOnly: false,
+        className: 'o'
+    });
+    $("body c").highlight("p", {
+        wordsOnly: false,
+        className: 'p'
+    });
+    $("body c").highlight("q", {
+        wordsOnly: false,
+        className: 'q'
+    });
+    $("body c").highlight("r", {
+        wordsOnly: false,
+        className: 'r'
+    });
+    $("body c").highlight("s", {
+        wordsOnly: false,
+        className: 's'
+    });
+    $("body c").highlight("t", {
+        wordsOnly: false,
+        className: 't'
+    });
+    $("body c").highlight("u", {
+        wordsOnly: false,
+        className: 'u'
+    });
+    $("body c").highlight("v", {
+        wordsOnly: false,
+        className: 'v'
+    });
+    $("body c").highlight("w", {
+        wordsOnly: false,
+        className: 'w'
+    });
+    $("body c").highlight("x", {
+        wordsOnly: false,
+        className: 'x'
+    });
+    $("body c").highlight("y", {
+        wordsOnly: false,
+        className: 'y'
+    });
+    $("body c").highlight("z", {
+        wordsOnly: false,
+        className: 'z'
+    });
+    //characters: '0123456789'
+    $("body c").highlight("0", {
+        wordsOnly: false,
+        className: '0'
+    });
+    $("body c").highlight("1", {
+        wordsOnly: false,
+        className: '1'
+    });
+    $("body c").highlight("2", {
+        wordsOnly: false,
+        className: '2'
+    });
+    $("body c").highlight("3", {
+        wordsOnly: false,
+        className: '3'
+    });
+    $("body c").highlight("4", {
+        wordsOnly: false,
+        className: '4'
+    });
+    $("body c").highlight("5", {
+        wordsOnly: false,
+        className: '5'
+    });
+    $("body c").highlight("6", {
+        wordsOnly: false,
+        className: '6'
+    });
+    $("body c").highlight("7", {
+        wordsOnly: false,
+        className: '7'
+    });
+    $("body c").highlight("8", {
+        wordsOnly: false,
+        className: '8'
+    });
+    $("body c").highlight("9", {
+        wordsOnly: false,
+        className: '9'
+    });
+});
+
+
